19e30519f20c9a7d2ac02c5f3d84fb0e071b966f
Robert Wang <robert.lu.wang@gmail.com>
2018-02-11 06:15:44 -0500

    Update centos-os.sh

--- dc-vagrant-cloud/scripts/centos-os.sh
+++ dc-vagrant-cloud/scripts/centos-os.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#  centos7 openstack with packstack provision script for packer  
+#  centos openstack with packstack provision script for packer  
 #  Robert Wang
 #  Jan 22th, 2018
+
+set -x
 
 # step0 presetup
 systemctl disable firewalld
@@ -23,11 +25,20 @@ yum install -y openstack-packstack
 packstack --gen-answer-file=packstack_`date +"%Y-%m-%d"`.conf
 cp packstack_`date +"%Y-%m-%d"`.conf latest_packstack.conf
 
-sed -i '/CONFIG_DEFAULT_PASSWORD=/c CONFIG_DEFAULT_PASSWORD=demo' latest_packstack.conf
-sed -i '/CONFIG_KEYSTONE_ADMIN_PW=/c CONFIG_KEYSTONE_ADMIN_PW=demo' latest_packstack.conf
-sed -i '/CONFIG_KEYSTONE_DEMO_PW=/c CONFIG_KEYSTONE_DEMO_PW=demo' latest_packstack.conf
-sed -i '/CONFIG_SWIFT_INSTALL=/c CONFIG_SWIFT_INSTALL=n' latest_packstack.conf
- 
-packstack --answer-file latest_packstack.conf || echo "packstack exited $? and is suppressed."
+sed -i '/^#CONFIG_DEFAULT_PASSWORD=/c CONFIG_DEFAULT_PASSWORD=demo' latest_packstack.conf
+sed -i '/^CONFIG_DEFAULT_PASSWORD=/c CONFIG_DEFAULT_PASSWORD=demo' latest_packstack.conf
+
+sed -i '/^#CONFIG_KEYSTONE_ADMIN_PW=/c CONFIG_KEYSTONE_ADMIN_PW=demo' latest_packstack.conf
+sed -i '/^CONFIG_KEYSTONE_ADMIN_PW=/c CONFIG_KEYSTONE_ADMIN_PW=demo' latest_packstack.conf
+
+sed -i '/^#CONFIG_KEYSTONE_DEMO_PW=/c CONFIG_KEYSTONE_DEMO_PW=demo' latest_packstack.conf
+sed -i '/^CONFIG_KEYSTONE_DEMO_PW=/c CONFIG_KEYSTONE_DEMO_PW=demo' latest_packstack.conf
+
+sed -i '/^#CONFIG_SWIFT_INSTALL=/c CONFIG_SWIFT_INSTALL=n' latest_packstack.conf
+sed -i '/^CONFIG_SWIFT_INSTALL=/c CONFIG_SWIFT_INSTALL=n' latest_packstack.conf
+
+packstack --answer-file latest_packstack.conf  || echo "packstack exited $? and is suppressed."
+
+
 
 
