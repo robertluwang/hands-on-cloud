3e72bd64e6115d0ce80a1337aae5b9d16e2fc569
Robert Wang <robert.lu.wang@gmail.com>
2018-09-26 07:49:22 -0400

    Update rhel7osp10-repo.sh

--- dc-vagrant-cloud/scripts/rhel7osp10-repo.sh
+++ dc-vagrant-cloud/scripts/rhel7osp10-repo.sh
@@ -16,6 +16,5 @@ subscription-manager repos --enable=rhel-7-server-extras-rpms
 subscription-manager repos --enable=rhel-7-server-openstack-10-rpms
 subscription-manager repos --enable=rhel-7-server-openstack-10-devtools-rpms
 
-yum install -y deltarpm
 yum install -y yum-utils  
 yum update -y 
