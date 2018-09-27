6bd17e8b947e899506e631093fc7e4a9391d2025
Robert Wang <robert.lu.wang@gmail.com>
2018-09-27 16:18:17 -0400

    Update rhel7-pack-update.sh

--- dc-vagrant-cloud/scripts/rhel7-pack-update.sh
+++ dc-vagrant-cloud/scripts/rhel7-pack-update.sh
@@ -107,13 +107,13 @@ else
     echo vagrant | packstack --answer-file latest_packstack.conf --timeout=1800 || echo "packstack exited $? and is suppressed."
 fi 
 
-sed -i "/export\ OS_AUTH_URL=/c export\ OS_AUTH_URL=http://$natnetip:5000/v3" /root/keystonerc_*
+sed -i "/export\ OS_AUTH_URL=/c export\ OS_AUTH_URL=http://$osip:5000/v3" /root/keystonerc_*
 sed -i "/OS_PROJECT_DOMAIN_NAME/d" /root/keystonerc_*
 sed -i "/OS_IDENTITY_API_VERSION/d" /root/keystonerc_*
 sed -i "$ a export\ OS_PROJECT_DOMAIN_NAME=Default" /root/keystonerc_*
 sed -i "$ a export\ OS_IDENTITY_API_VERSION=3" /root/keystonerc_*
 
-sed -i "/export\ OS_AUTH_URL=/c export\ OS_AUTH_URL=http://$natnetip:5000/v3" /home/vagrant/keystonerc_*
+sed -i "/export\ OS_AUTH_URL=/c export\ OS_AUTH_URL=http://$osip:5000/v3" /home/vagrant/keystonerc_*
 sed -i "/OS_PROJECT_DOMAIN_NAME/d" /home/vagrant/keystonerc_*
 sed -i "/OS_IDENTITY_API_VERSION/d" /home/vagrant/keystonerc_*
 sed -i "$ a export\ OS_PROJECT_DOMAIN_NAME=Default" /home/vagrant/keystonerc_*
