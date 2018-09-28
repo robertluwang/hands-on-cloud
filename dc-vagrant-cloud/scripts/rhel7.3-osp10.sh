ddb50b984e1958083d17a424babb6add50506132
Robert Wang <robert.lu.wang@gmail.com>
2018-09-28 12:47:05 -0400

    Update rhel7.3-osp10.sh

--- dc-vagrant-cloud/scripts/rhel7.3-osp10.sh
+++ dc-vagrant-cloud/scripts/rhel7.3-osp10.sh
@@ -83,8 +83,8 @@ $CONFIGSET CONFIG_SSL_CERT_DIR /root/packstackca
 
 packstack --answer-file latest_packstack.conf --timeout=1800 || echo "packstack exited $? and is suppressed."
 
-sed -i "/export\ OS_AUTH_URL=/c export\ OS_AUTH_URL=http://$osip:5000/v2" /root/keystonerc_*
-sed -i "/export\ OS_AUTH_URL=/c export\ OS_AUTH_URL=http://$osip:5000/v2" /home/vagrant/keystonerc_*
+sed -i "/export\ OS_AUTH_URL=/c export\ OS_AUTH_URL=http://$osip:5000/v2.0" /root/keystonerc_*
+sed -i "/export\ OS_AUTH_URL=/c export\ OS_AUTH_URL=http://$osip:5000/v2.0" /home/vagrant/keystonerc_*
 sed  -i "s/^[ \t]*//" /root/keystonerc_*
 sed  -i "s/^[ \t]*//" /home/vagrant/keystonerc_*
 cp /root/keystonerc_* /home/vagrant
