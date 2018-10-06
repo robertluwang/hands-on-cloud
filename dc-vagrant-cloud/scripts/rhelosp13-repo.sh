93c813ec982355bddc948a4d73f9b8486bb8d044
Robert Wang <robert.lu.wang@gmail.com>
2018-10-06 16:13:00 -0400

    Update rhelosp13-repo.sh

--- dc-vagrant-cloud/scripts/rhelosp13-repo.sh
+++ dc-vagrant-cloud/scripts/rhelosp13-repo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# rhel7osp10-repo.sh
-# rhel7.3 repo register script
+# rhel7osp-repo.sh
+# rhel7 repo register script
 # Robert Wang @github.com/robertluwang
 # Sept 24th, 2018 
 # $1 - rhel dev account user
 # $2 - rhel dev account password
+# $3 - OSP version 
 subscription-manager register --username $1 --password $2 --auto-attach
 poolid=`subscription-manager list --available --all|grep "Pool ID"|awk -F: '{print $2}'`
 for id in $poolid; do subscription-manager attach --pool=$id; done  
@@ -13,8 +14,8 @@ subscription-manager repos --disable=*
 subscription-manager repos --enable=rhel-7-server-rpms
 subscription-manager repos --enable=rhel-7-server-rh-common-rpms
 subscription-manager repos --enable=rhel-7-server-extras-rpms
-subscription-manager repos --enable=rhel-7-server-openstack-13-rpms
-subscription-manager repos --enable=rhel-7-server-openstack-13-devtools-rpms
+subscription-manager repos --enable=rhel-7-server-openstack-$3-rpms
+subscription-manager repos --enable=rhel-7-server-openstack-$3-devtools-rpms
 
 yum install -y yum-utils  
 
